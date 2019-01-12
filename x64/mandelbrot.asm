4fb5bb5ac4016bbb8927d74a940c04b33b2ecd70
skalermo <romnmko@gmail.com>
2019-01-12 23:43:30 +0100

    finished 64-bit version

--- x64/mandelbrot.asm
+++ x64/mandelbrot.asm
@@ -25,91 +25,69 @@ global  mandelbrot
 	
 	%define 	pallette_size 	15
 
-	%define 	y_step 	[ebp+48] ; double
-	%define 	x_step 	[ebp+40] ; double
-	%define 	y_start 	[ebp+32] ; double
-	%define 	x_start 	[ebp+24] ; double
-	%define 	max_iter 	[ebp+20] ; int
-	%define 	height 	[ebp+16] ; int
-	%define 	width 	[ebp+12] ; int 
-	%define 	buf 	[ebp+8] ; unsigned int*
-	
-	%define 	iterations [ebp-4] ; current iterations
+	%define 	y_step 		[rbp+24] ; double
+	%define 	x_step 		[rbp+16] ; double
+	%define 	x_start 	[rbp-8] ; double
+	%define 	max_iter 	rcx ; int
+	%define 	height 		rdx ; int
+	%define 	width 		rsi ; int 
 
-	; rbx - pointer of surface->pixels
-	; rcx - counter in main loop
-	; xmm0 - x coordinate of current pixel (on the complex plane)
-	; xmm1 - y coordinate of current pixel (on the complex plane)
-	; xmm2 - x variable for calculations
-	; xmm3 - y variable for calculations
-	; xmm4 - x*x
-	; xmm5 - y*y
-	; xmm6 - xtemp
-	; see algorithm on wiki: https://en.wikipedia.org/wiki/Mandelbrot_set
-	
 mandelbrot:
-	push 	ebp
-	mov 	ebp, esp
-	sub 	esp, 4 ; allocate memory for local variables
-	push 	rbx
- 
- 	mov 	rbx, buf ; copy pointer of surface->pixels to rbx
-	mov 	rcx, height ; rcx = height
-	mov 	rdx, width
-	movsd 	xmm1, y_start
-	xor 	rdx, rdx
-
+	push 	rbp
+	mov 	rbp, rsp
+	sub 	rsp, 8 
+	mov 	r9, height ; r9 = height
+	movsd 	x_start, xmm0
 loop_y:
-	push 	rcx ; current height onto stack
-	mov 	rcx, width ;
+	; push 	r9 ; current height onto stack
+	mov 	r10, width ;
 	movsd 	xmm0, x_start
 
 loop_x:
-	xorpd 	xmm2, xmm2 ; x = 0
-	xorpd 	xmm3, xmm3 ; y = 0
-	mov 	dword iterations, 0x0000 ; iter = 0
+	xorpd 	xmm4, xmm4 ; x = 0
+	xorpd 	xmm5, xmm5 ; y = 0
+	xor 	r8, r8
 
 main_loop:
-	movsd 	xmm7, x_step
-	movsd 	xmm4, xmm2 ;
-	mulsd 	xmm4, xmm4 ; x*x
-	movsd 	xmm5, xmm3
-	mulsd 	xmm5, xmm5 ; y*y
-	movsd 	xmm6, xmm4 ; xtemp = x*x
-	subsd 	xmm6, xmm5 ; xtemp -= y*y
-	addsd 	xmm6, xmm0 ; xtemp += x0
-	mulsd 	xmm3, xmm2 ; y *= x
-	addsd 	xmm3, xmm3 ; y *= 2
-	addsd 	xmm3, xmm1 ; y += y0
-	movsd 	xmm2, xmm6 ; x = xtemp
-	inc 	dword iterations ; iter++
-	mov 	rax, 0xffff
-	movsd 	xmm6, xmm4 ; x*x
-	addsd 	xmm6, xmm5 ; x*x + y*y
-	ucomisd	xmm6, [four] ; compare x*x + y*y with 4.0	
-	cmovnb	rax, iterations ; if x*x+y*y <= 4 then rax = current iter
-	cmp 	rax, max_iter
+	movsd 	xmm6, xmm4 ; xmm6[0] = x
+	shufpd 	xmm6, xmm5, 0x0	; xmm6[1] = y 
+	mulpd 	xmm6, xmm6 ; xmm6[0] = x^2, xmm6[1] = y^2
+	movsd 	xmm7, xmm6
+	shufps 	xmm6, xmm6, 0x4e ; swap x^2 and y^2
+	subsd 	xmm7, xmm6
+	addsd 	xmm7, xmm0
+	mulsd 	xmm5, xmm4 ; y *= x
+	addsd 	xmm5, xmm5 ; y *= 2
+	addsd 	xmm5, xmm1 ; y += y0
+	movsd 	xmm4, xmm7 ; x = xtemp
+	inc 	r8 ; iter++
+	mov 	rax, 0xffffffff
+	movsd 	xmm7, xmm6 ; y*y
+	shufps 	xmm6, xmm6, 0x4e
+	addsd 	xmm7, xmm6 ; x*x + y*y
+	ucomisd	xmm7, [four] ; compare x*x + y*y with 4.0	
+	cmovnb	rax, r8 ; if x*x+y*y <= 4 then eax = current iter
+	cmp 	rax, rcx
 	jl 		main_loop
-	mov 	rax, iterations
-	mov 	rdx, rax
+	mov 	rax, r8
 	and 	rax, pallette_size
-	mov 	rax, [pallette+rax*4] ;; temporary solution
-	mov 	rsi, 0x000000
-	cmp 	rdx, max_iter
-	cmove 	rax, rsi 
-	mov 	dword [rbx], rax ; load color info of current pixel to the buffer
-	mov 	rax, iterations
-	addsd 	xmm0, x_step ; x coordinate of next point
-	add 	rbx, 4 ; address of next pixel
-	dec 	rcx
+	mov 	eax, [pallette+eax*4] ;; temporary solution
+	mov 	r11, 0x000000	
+	cmp 	r8, rcx
+	cmove 	rax, r11
+	mov 	dword [rdi], eax ; load color info of current pixel to the buffer
+	mov 	rax, r8
+	addsd 	xmm0, xmm2 ; x coordinate of next point
+	add 	rdi, 4 ; address of next pixel
+	dec 	r10
 	jnz 	loop_x
-	subsd 	xmm1, y_step ; y coordinate of next point
-	pop 	rcx
-	dec 	rcx
+
+	subsd 	xmm1, xmm3 ; y coordinate of next point
+	dec 	r9
 	jnz 	loop_y
 	
 epilog:
-	pop 	rbx
-	mov 	esp, ebp
-	pop 	ebp
+	; pop 	rbx
+	mov 	rsp, rbp
+	pop 	rbp
 	ret
