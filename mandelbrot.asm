f38a8a369b4004e1760cfa2bb6530dafe383ea59
skalermo <skalermo@gmail.com>
2019-10-07 22:43:04 +0200

    Reorganize project structure

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -18,7 +18,22 @@
 	; 0x6a3403 ; brown 2
 
 section .data
-	pallette:	dd 	0x421e0f, 0x19071a, 0x09012f, 0x040449, 0x000764, 0x0c2c8a, 0x1852b1, 0x397dd1, 0x86b5e5, 0xd3ecf8, 0xf1e9bf, 0xf8c95f, 0xffaa00, 0xcc8000, 0x995700, 0x6a3403
+	pallette:	dd 	0x421e0f, \
+	                0x19071a, \
+	                0x09012f, \
+	                0x040449, \
+	                0x000764, \
+	                0x0c2c8a, \
+	                0x1852b1, \
+	                0x397dd1, \
+	                0x86b5e5, \
+	                0xd3ecf8, \
+	                0xf1e9bf, \
+	                0xf8c95f, \
+	                0xffaa00, \
+	                0xcc8000, \
+	                0x995700, \
+	                0x6a3403
 	four: 	dq -4.0
 section	.text
 global  mandelbrot
@@ -85,7 +100,6 @@ main_loop:
 	jnz 	loop_y
 	
 epilog:
-	; pop 	rbx
 	mov 	rsp, rbp
 	pop 	rbp
 	ret
